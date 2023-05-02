2e81066d7b98fae07b084bf9cf0c14aba3bb7b2f
jreid757 <jcreid1@gmail.com>
2023-05-02 19:46:01 +0000

    jreid-522023

---
+++ EC2_random_name_generator.py
@@ -0,0 +1,12 @@
+# Random EC2 name generator
+
+# Import the random module
+import random
+
+# Get user input for number of EC2 instances
+num = input("How many EC2 instances do you need names for?")
+name = input("What is the name of your department?")
+print("")
+
+# Uses user input to print out information
+print("The name of your EC2 instance is
